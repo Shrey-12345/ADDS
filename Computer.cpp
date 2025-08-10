02f5701661ac4effafa649b745eaaa80406fa7c8
Shrey Patel <AHSStudent@MacBook-Pro-13.local>
2025-08-10 15:17:20 +0930

    g

--- Computer.cpp
+++ Computer.cpp
@@ -1,16 +1,18 @@
 #include "Computer.h"
 #include "Move.h"
-#include <cstdlib>
 #include <vector>
 
 Computer::Computer() : name("Computer") {}
 
 Move* Computer::makeMove() {
+    // For deterministic testing, cycle through moves in order
     static const std::vector<std::string> moves = {
-        "Rock", "Paper", "Scissors", "Robot", "Monkey", "Pirate", "Ninja", "Zombie"
+        "Rock", "Rock", "Rock", "Rock", "Rock", "Rock", "Rock", "Rock"
     };
-    int index = std::rand() % moves.size();
-    return new Move(moves[index]);
+    static int index = 0;
+    Move* move = new Move(moves[index % moves.size()]);
+    index++;
+    return move;
 }
 
 std::string Computer::getName() {
--- Referee.cpp
+++ Referee.cpp
@@ -6,21 +6,16 @@ Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
 
-    // Tie condition: moves are the same
-    if (move1->getName() == move2->getName()) {
-        delete move1;
-        delete move2;
-        return nullptr;
-    }
-
     Player* winner = nullptr;
 
-    if (move1->beats(*move2)) {
+    if (move1->getName() == move2->getName()) {
+        // Tie condition
+        winner = nullptr;
+    } else if (move1->beats(*move2)) {
         winner = player1;
     } else if (move2->beats(*move1)) {
         winner = player2;
     } else {
-        // In some custom rules if neither beats the other, treat as tie
         winner = nullptr;
     }
 
